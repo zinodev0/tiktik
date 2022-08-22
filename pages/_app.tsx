36d6a91af6f3baba21ef8e48b92010dcc40c8928
zinodev0 <zinodev0@gmail.com>
2022-08-22 07:09:29 +0100

    Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -13,10 +15,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div>
-      Navbar
+      <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-          Sidebar
+          <Sidebar />
         </div>
 
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
