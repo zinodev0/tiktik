571045ad02ae2fe9f460f901966ca5438a16514f
zinodev0 <zinodev0@gmail.com>
2022-08-23 09:20:55 +0100

    Added Sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from "next";
+import axios from "axios";
+import { BASE_URL } from "../utils";
 
-const Home: NextPage = () => {
+// interface IProps{
+//   videos:
+// }
+
+const Home: NextPage = ({ videos }) => {
+  console.log(videos);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -9,3 +17,21 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () =>
+  //   {
+  //   query: { topic },
+  // }: {
+  //   query: { topic: string };
+  // }
+  {
+    let response = await axios.get(`${BASE_URL}/api/post`);
+
+    // if (topic) {
+    //   response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+    // }
+
+    return {
+      props: { videos: response.data },
+    };
+  };
